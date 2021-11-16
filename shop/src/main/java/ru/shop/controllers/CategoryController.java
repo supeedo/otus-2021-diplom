8cf51c274dae6a52338a80e69ccc057bd91f9aae
Sergey Yakovlev <web-wind@ya.ru>
2021-11-16 15:24:10 +0300

    add controller test

--- shop/src/main/java/ru/shop/controllers/CategoryController.java
+++ shop/src/main/java/ru/shop/controllers/CategoryController.java
@@ -1,7 +1,13 @@
 package ru.shop.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CategoryController {
+
+    @GetMapping("/")
+    public String getCategories(){
+        return "Hello";
+    }
 }
