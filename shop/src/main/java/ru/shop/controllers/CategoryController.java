8e8e0698d225d80ef4d912d34df31161575c4b9c
Sergey Yakovlev <web-wind@ya.ru>
2021-11-16 16:50:04 +0300

    add openapi 3

--- shop/src/main/java/ru/shop/controllers/CategoryController.java
+++ shop/src/main/java/ru/shop/controllers/CategoryController.java
@@ -1,12 +1,17 @@
 package ru.shop.controllers;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestController
+@RestControllerAdvice
+@Tag( name = "Тестовое имя контроллера", description = "Тестовое описание контроллера")
 public class CategoryController {
 
     @GetMapping("/")
+//    @ApiOperation("Описание метода")
     public String getCategories(){
         return "Hello";
     }
