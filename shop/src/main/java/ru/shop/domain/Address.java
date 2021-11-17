f7212f5252355c73aba727206852144bffd05d81
Sergey Yakovlev <web-wind@ya.ru>
2021-11-17 19:06:57 +0300

    add entity annotation

--- shop/src/main/java/ru/shop/domain/Address.java
+++ shop/src/main/java/ru/shop/domain/Address.java
@@ -1,10 +1,97 @@
 package ru.shop.domain;
 
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "address")
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "city", nullable = false)
     private String city;
+    @Column(name = "street", nullable = false)
     private String street;
+    @Column(name = "house_number", nullable = false)
     private String houseNumber;
+    @Column(name = "floor", nullable = false)
     private Long floor;
+    @Column(name = "apartment_number")
     private String apartmentNumber;
+
+    public Address() {
+    }
+
+    public Address(Long id, String city, String street, String houseNumber, Long floor, String apartmentNumber) {
+        this.id = id;
+        this.city = city;
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.floor = floor;
+        this.apartmentNumber = apartmentNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public Long getFloor() {
+        return floor;
+    }
+
+    public void setFloor(Long floor) {
+        this.floor = floor;
+    }
+
+    public String getApartmentNumber() {
+        return apartmentNumber;
+    }
+
+    public void setApartmentNumber(String apartmentNumber) {
+        this.apartmentNumber = apartmentNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(id, address.id) && Objects.equals(city, address.city) && Objects.equals(street, address.street) && Objects.equals(houseNumber, address.houseNumber) && Objects.equals(floor, address.floor) && Objects.equals(apartmentNumber, address.apartmentNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, city, street, houseNumber, floor, apartmentNumber);
+    }
 }
--- shop/src/main/java/ru/shop/domain/Role.java
+++ shop/src/main/java/ru/shop/domain/Role.java
@@ -1,6 +1,62 @@
 package ru.shop.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "roles")
 public class Role {
+    @Id
+    @Column(name = "id")
     private Long id;
+    @Column(name = "role", nullable = false, unique = true)
     private String roleName;
+
+    public Role() {
+    }
+
+    public Role(Long id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id) && Objects.equals(roleName, role.roleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, roleName);
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", roleName='" + roleName + '\'' +
+                '}';
+    }
 }
