95c0ac4e74b33efdf640546460039180e13081b3
Sergey Yakovlev <web-wind@ya.ru>
2021-11-17 23:39:13 +0300

    add UserInformationDTO

---
+++ shop/src/main/java/ru/shop/domain/UserInformationDTO.java
@@ -0,0 +1,97 @@
+package ru.shop.domain;
+
+import java.util.Objects;
+
+public class UserInformationDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String patronymic;
+    private String phone;
+    private AddressDTO address;
+
+    public UserInformationDTO() {
+    }
+
+    public UserInformationDTO(Long id, String firstName, String lastName, String patronymic, String phone, AddressDTO address) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.patronymic = patronymic;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public AddressDTO getAddress() {
+        return address;
+    }
+
+    public void setAddress(AddressDTO address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserInformationDTO that = (UserInformationDTO) o;
+        return Objects.equals(id, that.id) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(patronymic, that.patronymic) && Objects.equals(phone, that.phone) && Objects.equals(address, that.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, patronymic, phone, address);
+    }
+
+    @Override
+    public String toString() {
+        return "UserInformationDTO{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", patronymic='" + patronymic + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
